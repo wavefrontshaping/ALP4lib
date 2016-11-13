f7631879e6a3c2de84d5efcd535bfdc26cfc9753
wavefronthsaping <admin@wavefrontshaping.net>
2016-11-13 09:07:32 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+import os
+
+long_description='Please read the documentation on Github and official Vialux documentation.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+    name = "ALP4lib",
+    version = "0.0.2",
+    author = "Sebastien Popoff",
+    author_email = "sebastien.popoff@espci.fr",
+    description = ("A module to control Vialux DMDs based on ALP4.X API."
+                   "It uses the .ddl files provided by Vialux."),
+    license = "MIT",
+    keywords = "DMD Vialux",
+    url = "https://github.com/wavefronthsaping/ALP4lib",
+    py_modules = ['ALP4'],
+    long_description=long_description,
+    classifiers=[
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
